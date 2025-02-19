2ace0163601b0ae281638e34d345812ca950f1b7
egomylego <egomylego1761@gmail.com>
2025-02-18 17:29:38 -0800

    Update Universal_Shift2Run.lua

--- Universal_Shift2Run.lua
+++ Universal_Shift2Run.lua
@@ -16,6 +16,7 @@ end
 local player = game.Players.LocalPlayer
 local character = player.Character
 local humanoid = character:WaitForChild("Humanoid")
+local sprintspeed = nil
 
 local sprinting = false
 
@@ -31,10 +32,76 @@ game:GetService("UserInputService").InputEnded:Connect(function(inputObject, gam
 	end
 end)
 
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local speedlab = Instance.new("TextLabel")
+local speedbox = Instance.new("TextBox")
+local UICorner = Instance.new("UICorner")
+
+-- Properties
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.Active = true
+Frame.BackgroundColor3 = Color3.new(0.0980392, 0.0980392, 0.0980392)
+Frame.BorderColor3 = Color3.new(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.175155923, 0, 0.387387395, 0)
+Frame.Selectable = true
+Frame.Size = UDim2.new(0, 431, 0, 257)
+Frame.Style = Enum.FrameStyle.RobloxRound
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel.BackgroundTransparency = 1
+TextLabel.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(-0.0208816715, 0, 0, 0)
+TextLabel.Size = UDim2.new(0, 431, 0, 55)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = "SHIFT2RUN SPEED CHANGER"
+TextLabel.TextColor3 = Color3.new(1, 1, 1)
+TextLabel.TextSize = 37
+TextLabel.TextWrapped = true
+
+speedlab.Name = "speedlab"
+speedlab.Parent = Frame
+speedlab.BackgroundColor3 = Color3.new(1, 1, 1)
+speedlab.BackgroundTransparency = 1
+speedlab.BorderColor3 = Color3.new(0, 0, 0)
+speedlab.BorderSizePixel = 0
+speedlab.Position = UDim2.new(0.303944319, 0, 0.249027237, 0)
+speedlab.Size = UDim2.new(0, 169, 0, 35)
+speedlab.Font = Enum.Font.SourceSansBold
+speedlab.Text = "SPEED"
+speedlab.TextColor3 = Color3.new(1, 1, 1)
+speedlab.TextScaled = true
+speedlab.TextSize = 14
+speedlab.TextWrapped = true
+
+speedbox.Name = "speedbox"
+speedbox.Parent = Frame
+speedbox.BackgroundColor3 = Color3.new(1, 1, 1)
+speedbox.BorderColor3 = Color3.new(0, 0, 0)
+speedbox.BorderSizePixel = 0
+speedbox.Position = UDim2.new(0.276471853, 0, 0.399360508, 0)
+speedbox.Size = UDim2.new(0, 191, 0, 33)
+speedbox.Font = Enum.Font.SourceSans
+speedbox.Text = "30"
+speedbox.TextColor3 = Color3.new(0, 0, 0)
+speedbox.TextScaled = true
+speedbox.TextSize = 14
+speedbox.TextWrapped = true
+
 while true do
 	wait(0.1)
+	task.wait(0.01)
+	sprintspeed = speedbox.Text
 	if sprinting then
-		humanoid.WalkSpeed = 30
+		humanoid.WalkSpeed = sprintspeed
 	else
 		humanoid.WalkSpeed = 16
 	end
